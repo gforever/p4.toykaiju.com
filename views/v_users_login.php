20f87f9ac5602ed53f29daaefeb8a5429aa6cb41
Andrew Wong <awong@fas.harvard.edu>
2013-12-14 11:46:53 -0500

    login error checking testing

--- views/v_users_login.php
+++ views/v_users_login.php
@@ -2,18 +2,20 @@
 
 <form method='POST' action='/users/p_login'>
 
-  	<p>Email</p>
-    <input type='text' name='email'>    
-	<p>Password</p>
-    <input type='password' name='password'>
-	<p>
-    <?php if(isset($error)): ?>
-        <div class='error'>
-            Login failed. Please double check your email and password.
-        </div>
-    <?php endif; ?>
-	</p>
-    <input type='submit' value='Log in'>
+    <p>Email</p>
+    <input type='text' name='email' required>    
+
+    <p>Password</p>
+    <input type='password' name='password' required>
     
+    <?php if(isset($error)): ?>
+         	<div class='error'>
+            	Login failed. Please double check your email and password.
+        	</div>
+    
+    <?php endif; ?>
+
+    <p><input type='submit' value='Log in'></p>
+
 </form>
 

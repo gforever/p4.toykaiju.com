1d54f7d98d999bd4869abd138a78d6463b947a2a
Andrew Wong <awong@fas.harvard.edu>
2013-12-18 21:21:08 -0500

    testing jquery

--- views/v_posts_index.php
+++ views/v_posts_index.php
@@ -2,11 +2,19 @@
 	   Router::redirect('/users/login');
 	} ?>    
     
-<?php foreach($posts as $post): ?>
+
+
+  <script>
+  $(function() {
+    $( "#sortable" ).sortable();
+    $( "#sortable" ).disableSelection();
+  });
+  </script>
 
 <div class="postCanvas">
                        
 <ul id="sortable">
+<?php foreach($posts as $post): ?>
  <!--Displays the user's name -->
  <!--      <span class="uName">
 		   < ?=$post['first_name'] .': '?> 
@@ -20,10 +28,10 @@
               <?=" : ".$post['content']?> 
           </span>                                   
        <!--If the user id  matches with the user who made the post, then provide the option of editing or deleting entry -->
-		<?php if($user->user_id == $post['post_user_id']): ?>                
-         <a href=/posts/edit/<?=$post['post_id']?> class="editDel">Edit</a> - 
-         <a href=/posts/delete/<?=$post['post_id']?> class="editDel">Delete</a>               
-        <?php endif; ?> 
+		<!--< ?php if($user->user_id == $post['post_user_id']): ?>                
+         <a href=/posts/edit/< ?=$post['post_id']?> class="editDel">Edit</a> - 
+         <a href=/posts/delete/< ?=$post['post_id']?> class="editDel">Delete</a>     -->
+        < ?php endif; ?> 
                                   
   <!--Displays the time -->
   <!--  <div class="postDate"> 

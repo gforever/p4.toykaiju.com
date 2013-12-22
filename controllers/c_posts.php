d5519716720143a78b3784cea6b6f822a36307c9
Andrew Wong <awong@fas.harvard.edu>
2013-12-22 12:27:24 -0500

    uncomment out ORDER BY

--- controllers/c_posts.php
+++ controllers/c_posts.php
@@ -264,10 +264,10 @@ public function processsortable(){
 
         INNER JOIN users 
            ON posts.user_id = users.user_id
-        WHERE users.user_id = '.$this->user->user_id; 
-		#ORDER BY 
-		#	posts.ranking ASC,
-		#	posts.created DESC';		
+        WHERE users.user_id = '.$this->user->user_id.'
+		ORDER BY 
+			posts.ranking ASC,
+			posts.created DESC';		
 		
 		//Only allow logged in user to see his/her own tasks (line 267).  Join will connect posts to the user id. We will arrange the order by rank and date created. 	
 
